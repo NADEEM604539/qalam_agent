2019a9254a1367a51981faf69e490dc5f869b9ad
NADEEM604539 <ndm604539@gmail.com>
2026-08-20 19:04:32 +0500

    chaecking flow

--- backend/src/backend/routers/login.py
+++ backend/src/backend/routers/login.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from backend.DTO.login import Login_request
 from backend.services.login import login, register, get_user
-# from backend.web_scraping.playwright_login import login_to_qalam
+from backend.web_scraping.playwright_login import login_to_qalam
+from playwright.async_api import async_playwright, Page
+from backend.web_scraping.playwright_get_courses import get_enrolled_courses
 from fastapi import HTTPException
 
 router = APIRouter(
@@ -11,7 +13,7 @@ router = APIRouter(
 
 @router.post("/")
 async def login_Request(request: Login_request):
-    # qalam_login = await login_to_qalam(email=request.email, password=request.password)
+    qalam_login = await login_to_qalam(page=Page, email=request.email, password=request.password)
     qalam_login=True
     if qalam_login:
         user = await get_user(request.email)
@@ -28,3 +30,7 @@ async def login_Request(request: Login_request):
         
 
 
+@router.get("/testing")
+async def testing():
+    courses = await get_enrolled_courses()
+    return courses
